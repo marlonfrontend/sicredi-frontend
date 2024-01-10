46d08bef8be20efb4c8c6e1a22280badad1d637f
Marlon Felix <marlonsantos@kirvano.com>
2024-01-10 20:31:44 -0300

    chore: created input component

---
+++ src/components/ui/Input/Input.styles.ts
@@ -0,0 +1,7 @@
+import { tv } from 'tailwind-variants'
+
+export const inputStyles = tv({
+  slots: {
+    base: 'border rounded-lg',
+  },
+})
---
+++ src/components/ui/Input/Input.types.ts
@@ -0,0 +1,5 @@
+export type InputProps = {
+  label: string
+  value: string
+  onChange: (value: string) => void
+}
