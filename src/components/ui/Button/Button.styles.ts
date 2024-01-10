e535e554277cc3cd09c901576528746a8a7b39d7
Marlon Felix <marlonsantos@kirvano.com>
2024-01-10 20:31:03 -0300

    chore: use tailwind variants in button component

---
+++ src/components/ui/Button/Button.styles.ts
@@ -0,0 +1,7 @@
+import { tv } from 'tailwind-variants'
+
+export const buttonStyles = tv({
+  slots: {
+    base: 'bg-black px-6 py-2 rounded-lg',
+  },
+})
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,5 +1,8 @@
 import { PropsWithChildren } from 'react'
+import { buttonStyles } from './Button.styles'
 
 export const Button = ({ children }: PropsWithChildren) => {
-  return <button>{children}</button>
+  const { base } = buttonStyles()
+
+  return <button className={base()}>{children}</button>
 }
