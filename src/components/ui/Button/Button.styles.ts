ab671ca61ea200484b0eecc3726dc7e32f2f9dc6
Marlon Felix <marlonsantos@kirvano.com>
2024-01-11 18:20:04 -0300

    chore: updated button component

--- src/components/ui/Button/Button.styles.ts
+++
@@ -1,7 +0,0 @@
-import { tv } from 'tailwind-variants'
-
-export const buttonStyles = tv({
-  slots: {
-    base: 'bg-black px-6 py-2 rounded-lg',
-  },
-})
--- src/components/ui/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from 'react'
-import { buttonStyles } from './Button.styles'
-
-export const Button = ({ children }: PropsWithChildren) => {
-  const { base } = buttonStyles()
-
-  return <button className={base()}>{children}</button>
-}
