425c722739a5f58cf9d649fa17e7f2605b5e6979
Marlon Felix <marlonsantos@kirvano.com>
2024-01-12 13:36:52 -0300

    chore: add transition auth route

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,14 +3,30 @@
 import { PropsWithChildren } from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { AuthProvider } from '@/stores/Auth'
+import { motion } from 'framer-motion'
 
 export const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
     <NextThemesProvider attribute="class" defaultTheme="light">
       <AuthProvider>
-        <main className="flex h-screen flex-col items-center justify-center">
+        <motion.main
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              opacity: 0,
+            },
+            visible: {
+              opacity: 1,
+              transition: {
+                delay: 0.3,
+              },
+            },
+          }}
+          className="flex h-screen flex-col items-center justify-center"
+        >
           {children}
-        </main>
+        </motion.main>
       </AuthProvider>
     </NextThemesProvider>
   )
